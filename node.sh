3c47a8ae90c76084622e8fddedc178f1c064e6a5
mrdavidaylward <60498357+mrdavidaylward@users.noreply.github.com>
2020-04-24 20:03:51 +0200

    Update node.sh

--- node.sh
+++ node.sh
@@ -6,10 +6,10 @@ sudo adduser --no-create-home --disabled-login --shell /bin/false --gecos "Node 
 # Download node_exporter and copy utilities to where they should be in the filesystem
 #VERSION=0.16.0
 VERSION=$(curl https://raw.githubusercontent.com/prometheus/node_exporter/master/VERSION)
-wget https://github.com/prometheus/node_exporter/releases/download/v${VERSION}/node_exporter-${VERSION}.linux-amd64.tar.gz
-tar xvzf node_exporter-${VERSION}.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v${VERSION}/node_exporter-${VERSION}.linux-armhf.tar.gz
+tar xvzf node_exporter-${VERSION}.linux-armhf.tar.gz
 
-sudo cp node_exporter-${VERSION}.linux-amd64/node_exporter /usr/local/bin/
+sudo cp node_exporter-${VERSION}.linux-armhf/node_exporter /usr/local/bin/
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
 # systemd
@@ -20,5 +20,5 @@ sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
 
 # Installation cleanup
-rm node_exporter-${VERSION}.linux-amd64.tar.gz
-rm -rf node_exporter-${VERSION}.linux-amd64
+rm node_exporter-${VERSION}.linux-armhf.tar.gz
+rm -rf node_exporter-${VERSION}.linux-armhf
