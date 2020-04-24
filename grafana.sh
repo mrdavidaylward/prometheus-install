35efd9d774bba4d6f0f49c43c3512cf8309cee69
mrdavidaylward <60498357+mrdavidaylward@users.noreply.github.com>
2020-04-24 20:05:40 +0200

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Download grafana
-wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_4.6.3_amd64.deb
+wget https://dl.grafana.com/oss/release/grafana_6.7.3_armhf.deb
 
 # Install grafana
-sudo apt-get install -y adduser libfontconfig
-sudo dpkg -i grafana_4.6.3_amd64.deb
+sudo apt-get install -y adduser libfontconfig1
+
+sudo dpkg -i grafana_6.7.3_armhf.deb
 
 # systemd
 sudo systemctl daemon-reload
@@ -13,4 +14,4 @@ sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
 
 # Installation cleanup
-rm grafana_4.6.3_amd64.deb
+rm grafana_6.7.3_armhf.deb
